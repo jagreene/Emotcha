d3db2f2537c3a2182ea80068f81a1246d8e12210
jagreene <austin.greene@students.olin.edu>
2015-07-22 16:54:28 -0400

    trying to debug

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ module.exports = function(indico, fs){
     postImage: function(req, res){
       var string = String(req.body.data);
       string=string.split(",")[1];
-      indico.fer(string, {detect:true})
+      indico.fer(string)
       .then(function(data) {
         console.log(data)
         if (data.length === 1 && 'location' in data[0]){
