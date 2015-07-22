225aaa3ecbbdf51c035a10b3a2cdcf51107f7bbe
jagreene <austin.greene@students.olin.edu>
2015-07-22 15:18:21 -0400

    implemented indico fl

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -18,10 +18,8 @@ var updateInterval;
 
 //Initialize Canvas Elements
 var canvas = document.createElement('canvas');
-var faceCanvas = document.createElement('canvas');
 
 var ctx = canvas.getContext('2d');
-var ctxf = faceCanvas.getContext('2d');
 
 canvas.width = 450;
 canvas.height = 450;
@@ -82,16 +80,18 @@ $loginBtn.click(function(){
 
 function getExample() {
     //fetch picture and emotional profile from server
+    console.log("button pushed")
     $.get('/image')
     .done(function (data, status){
         exampleEmotions = data.emotions;
+        console.log(data.file)
         $exampleImg.attr('src', 'images/'+data.file);
-        $('#ex-angry').css('width', (data.emotions.Angry*100)+'%');
-        $('#ex-sad').css('width', (data.emotions.Sad*100)+'%');
-        $('#ex-neutral').css('width', (data.emotions.Neutral*100)+'%');
-        $('#ex-surprise').css('width', (data.emotions.Surprise*100)+'%');
-        $('#ex-fear').css('width', (data.emotions.Fear*100)+'%');
-        $('#ex-happy').css('width', (data.emotions.Happy*100)+'%');
+        $('#ex-angry').css('width', (exampleEmotions.Angry*100)+'%');
+        $('#ex-sad').css('width', (exampleEmotions.Sad*100)+'%');
+        $('#ex-neutral').css('width', (exampleEmotions.Neutral*100)+'%');
+        $('#ex-surprise').css('width', (exampleEmotions.Surprise*100)+'%');
+        $('#ex-fear').css('width', (exampleEmotions.Fear*100)+'%');
+        $('#ex-happy').css('width', (exampleEmotions.Happy*100)+'%');
     })
     .error(function (data, status){
         console.log(status);
@@ -105,88 +105,58 @@ function getSnapshot(){
     $(video).replaceWith(canvas);
     clearInterval(updateInterval);
     $('.submit-btn').children().replaceWith("<i class='mdi-av-play-arrow'></i>");
-    $(video).faceDetection({
-        complete: function (faces){
-            if(faces[0]){
-                faceCanvas.width = faces[0].width;
-                faceCanvas.height = faces[0].height;
-                ctxf.drawImage(canvas, faces[0].x, faces[0].y, faces[0].width, faces[0].height, 0, 0, faceCanvas.width, faceCanvas.height);
-                var dataURI = faceCanvas.toDataURL('image/jpg');
-                postImage(dataURI);
-            } else {
-                var dataURI = canvas.toDataURL('image/jpg');
-                postImage(dataURI);
-            }
-        },
-
-        error: function (code, message){
-            console.log("Face Error!", message);
-            var dataURI = canvas.toDataURL('image/jpg');
-            postImage(dataURI);
-        }
-    });
+    var dataURI = canvas.toDataURL('image/jpg');
+    postImage(dataURI);
 }
 
 function update(){
     //Grab image from video, find face, and send to server every second
     ctx.drawImage(video, 0, 0, 600, 450);
-    $(canvas).faceDetection({
-        complete: function (faces){
-            if(faces[0]){
-                faceCanvas.width = faces[0].width;
-                faceCanvas.height = faces[0].height;
-                ctxf.drawImage(canvas, faces[0].x, faces[0].y, faces[0].width, faces[0].height, 0, 0, faceCanvas.width, faceCanvas.height);
-                var dataURI = faceCanvas.toDataURL('image/jpg');
-                postImage(dataURI);
-            }
-        },
-
-        error: function (code, message){
-            console.log("Face Error!", message);
-        }
-    });
+    var dataURI = canvas.toDataURL('image/jpg');
+    postImage(dataURI);
 }
 
 function postImage(dataURI){
     //send face to server and respond by changing bars
     $.post("/image", {data: dataURI})
     .done(function (data, status) {
-        liveEmotions = data;
-        $('#lv-angry').css('width', (data.Angry*100)+'%');
-        $('#lv-sad').css('width', (data.Sad*100)+'%');
-        $('#lv-neutral').css('width', (data.Neutral*100)+'%');
-        $('#lv-surprise').css('width', (data.Surprise*100)+'%');
-        $('#lv-fear').css('width', (data.Fear*100)+'%');
-        $('#lv-happy').css('width', (data.Happy*100)+'%');
+        if (data['status'] === "found"){
+            data = data['data']
+            liveEmotions = data;
+            $('#lv-angry').css('width', (data.Angry*100)+'%');
+            $('#lv-sad').css('width', (data.Sad*100)+'%');
+            $('#lv-neutral').css('width', (data.Neutral*100)+'%');
+            $('#lv-surprise').css('width', (data.Surprise*100)+'%');
+            $('#lv-fear').css('width', (data.Fear*100)+'%');
+            $('#lv-happy').css('width', (data.Happy*100)+'%');
 
-        //Measure if emotions close enough to unlock system
-        diffs =[Math.abs(liveEmotions.Angry - exampleEmotions.Angry),
-                Math.abs(liveEmotions.Sad - exampleEmotions.Sad),
-                Math.abs(liveEmotions.Neutral - exampleEmotions.Neutral),
-                Math.abs(liveEmotions.Surprise - exampleEmotions.Surprise),
-                Math.abs(liveEmotions.Fear - exampleEmotions.Fear),
-                Math.abs(liveEmotions.Happy - exampleEmotions.Happy)]
+            //Measure if emotions close enough to unlock system
+            diffs =[Math.abs(liveEmotions.Angry - exampleEmotions.Angry),
+                    Math.abs(liveEmotions.Sad - exampleEmotions.Sad),
+                    Math.abs(liveEmotions.Neutral - exampleEmotions.Neutral),
+                    Math.abs(liveEmotions.Surprise - exampleEmotions.Surprise),
+                    Math.abs(liveEmotions.Fear - exampleEmotions.Fear),
+                    Math.abs(liveEmotions.Happy - exampleEmotions.Happy)]
 
-        console.log("Diffs:", diffs);
-        sum = 0;
-        for(i=0; i<diffs.length; i++){
-            sum+= diffs[i];
-        }
+            sum = 0;
+            for(i=0; i<diffs.length; i++){
+                sum+= diffs[i];
+            }
 
-        metric = 2 - sum;
+            metric = 2 - sum;
 
-        console.log("Metric:", metric);
-        if(metric>1.25){
-            $submitBtn.children('i').replaceWith("<i class='mdi-content-undo'></i>");
-            $submitBtn.toggleClass('red', false);
-            $submitBtn.toggleClass('green', true);
-            $loginBtn.toggleClass('disabled', false);
-            $('.how-to').toggleClass('hide', true);
-            capturing = false;
-            finished = true;
-            if(firstFinish){
-                alert("You've sucessfuly matched emotional profiles and unlocked the login button, tap the green arrow on your portrait to try again or the login button to complete the demo login");
-                firstFinish = false;
+            if(metric>1.5){
+                $submitBtn.children('i').replaceWith("<i class='mdi-content-undo'></i>");
+                $submitBtn.toggleClass('red', false);
+                $submitBtn.toggleClass('green', true);
+                $loginBtn.toggleClass('disabled', false);
+                $('.how-to').toggleClass('hide', true);
+                capturing = false;
+                finished = true;
+                if(firstFinish){
+                    alert("You've sucessfuly matched emotional profiles and unlocked the login button, tap the green arrow on your portrait to try again or the login button to complete the demo login");
+                    firstFinish = false;
+                }
             }
         }
     })
@@ -213,5 +183,5 @@ function captureVideo() {
     }
     $('.submit-btn').children().replaceWith("<i class='mdi-av-pause'></i>");
     capturing = true;
-    updateInterval = setInterval(update, 1000);
+    updateInterval = setInterval(update, 500);
 }
--- routes/index.js
+++ routes/index.js
@@ -11,25 +11,26 @@ module.exports = function(indico, fs){
     getImage: function(req, res){
       fs.readdir('public/images', function (err, imageNames){
         image = imageNames[Math.floor(Math.random()*imageNames.length)];
-        fs.readFile('public/images/'+image, function(err, data) {
-          string = new Buffer(data).toString('base64');
-          indico.fer(string)
-          .then(function(data) {
-            res.json({file: image, emotions: data});
-          }).catch(function(err) {
-            console.warn("Error:", err);
-          });
-        });
-      });
+        fs.readFile('public/example_emotions.json', 'utf8', function (err, data) {
+          emotions = JSON.parse(data)[image]
+          res.json({file: image, emotions: emotions});
+        })
+     })
     },
 
     //Recieve image from client and send response
     postImage: function(req, res){
       var string = String(req.body.data);
       string=string.split(",")[1];
-      indico.fer(string)
+      indico.fer(string, {detect:true, sensitivity:.8})
       .then(function(data) {
-        res.json(data);
+        if (data.length === 1 && 'location' in data[0]){
+          data = data[0]['emotions'];
+          status = "found"
+          res.json({data:data, status:status});
+        } else {
+          res.json({data:NaN, status:"not_found"})
+        }
       }).catch(function(err) {
         console.warn(err);
       });
